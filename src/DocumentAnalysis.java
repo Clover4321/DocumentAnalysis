4ec50c6d18255a0707b2004188d4481a3228a451
My Ubuntu <ultimecia@ubuntu.(none)>
2014-11-28 00:36:55 +0800

    use two round mapred

--- src/DocumentAnalysis.java
+++ src/DocumentAnalysis.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.StringTokenizer;
+import java.util.*;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
@@ -21,6 +23,8 @@ import org.apache.hadoop.mapreduce.TaskAttemptID;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
@@ -78,10 +82,13 @@ public class DocumentAnalysis {
 	        public boolean nextKeyValue() throws IOException,
 	        InterruptedException {
 	            if (fsin.getPos() < end) {
+
 	                if (readUntilMatch(startTag, false)) {
 	                    try {
+
 	                        buffer.write(startTag);
 	                        if (readUntilMatch(endTag, true)) {
+
 	                            key.set(fsin.getPos());
 	                            value.set(buffer.getData(), 0,
 	                                    buffer.getLength());
@@ -116,12 +123,15 @@ public class DocumentAnalysis {
 
 	        private boolean readUntilMatch(byte[] match, boolean withinBlock)
 	        throws IOException {
+
 	            int i = 0;
 	            while (true) {
+
 	                int b = fsin.read();
+
 	                // end of file:
 	                    if (b == -1)
-	                        return false;
+	                        {return false;}
 	                // save to buffer:
 	                    if (withinBlock)
 	                        buffer.write(b);
@@ -129,26 +139,24 @@ public class DocumentAnalysis {
 	                    if (b == match[i]) {
 	                        i++;
 	                        if (i >= match.length)
-	                            return true;
+	                            {return true;}
 	                    } else
 	                        i = 0;
 	                    // see if we've passed the stop point:
 	                    if (!withinBlock && i == 0 && fsin.getPos() >= end)
-	                        return false;
+	                        {return false;}
 	            }
 	        }
 	    }
 	}
 	
-	public static class Map extends 
-	Mapper<LongWritable, Text, Text, IntWritable>
+	public static class Map1 extends 
+	Mapper<LongWritable, Text, Text, Text>
 	{
-		private final static IntWritable one = new IntWritable(1);
-		private Text word = new Text();
-		
-		public void map(LongWritable key, Text value,
-				Context context)
-				throws IOException,InterruptedException
+		private Text mapresultkey = new Text();
+		private Text position = new Text();
+		public void map(LongWritable key, Text value,Context context)
+		       throws IOException,InterruptedException
 		{
 			XMLHandler xmlHandler = new XMLHandler();
 			//System.out.println(value.toString());
@@ -169,28 +177,98 @@ public class DocumentAnalysis {
 			String parsedText = textParser.regExpParse(pageText);
 			//System.out.println(parsedText);
 			StringTokenizer tokenizer = new StringTokenizer(parsedText);
+            int count_position=0;
 			while (tokenizer.hasMoreTokens())
             {
-                word.set(tokenizer.nextToken());
-                context.write(word, one);
+				count_position++;
+                mapresultkey.set(tokenizer.nextToken() + "," + pageId);
+                position.set(String.valueOf(count_position));
+                //System.out.println(mapresultkey+" "+position);
+                context.write(mapresultkey, position);
             }
 		}
 	}
 	
-	public static class Reduce extends 
-    Reducer<Text, IntWritable, Text, IntWritable>
+	public static class Reduce1 extends 
+    Reducer<Text, Text, Text, Text>
 	{
 		
-		public void reduce(Text key, Iterable<IntWritable> values,
+		public void reduce(Text key, Iterable<Text> values,
+		        Context context)
+		        throws IOException, InterruptedException{
+			    Text position = new Text();
+			    Text value = new Text();
+			    String pos_str="[";
+			    
+			    //count term frequency
+			    int tf = 0;
+			    ArrayList<Integer> arr = new ArrayList<Integer>();
+			    for (Text val : values)
+			    {
+			    	tf++;
+			    	arr.add(Integer.parseInt(val.toString()));
+			    }
+			    Collections.sort(arr);
+			    int flag = 0;
+			    int last_position =0;
+			    for(Integer i: arr)
+			    {
+			    	if(flag==0) {flag=1;last_position=i;pos_str += (i.toString()+",");}
+			    	else
+			    	{
+			    		pos_str += (String.valueOf((i.intValue()-last_position))+",");
+			    		last_position = i.intValue();
+			    	}
+			    }
+			    pos_str = pos_str.substring(0, pos_str.length()-1);
+			    pos_str += "]";
+			    position.set(pos_str);
+			    /*change key to word
+			    Text reduce_input_key = new Text();
+			    String word = key.toString();
+			    String[] splitter = word.split(",");
+			    reduce_input_key.set(splitter[0]);
+			    value.set(splitter[1]+":"+pos_str);
+			    System.out.println(reduce_input_key+"  "+splitter[1]+":"+position);
+			    */
+			    context.write(key, position);
+			    //System.out.println(key+" "+position);
+			}
+	} 
+	
+	public static class Map2 extends 
+	Mapper<Text, Text, Text, Text>
+	{
+		public void map(Text key, Text value,Context context)
+		       throws IOException,InterruptedException
+		{
+			//System.out.println("map2");
+			int splitIndex = key.toString().indexOf(",");  
+            context.write(new Text(key.toString().substring(0, splitIndex)),  
+                    new Text(key.toString().substring(splitIndex + 1) + ":"  
+                            + value.toString()));  
+		}
+	}
+	
+	public static class Reduce2 extends 
+    Reducer<Text, Text, Text, Text>
+	{
+		
+		public void reduce(Text key, Iterable<Text> values,
 		        Context context)
 		        throws IOException, InterruptedException
 			{
-			    int sum = 0;
-			    for (IntWritable val : values)
+			    Text output = new Text();
+			    String result = "";
+			    for (Text val : values)
 			    {
-			    	sum += val.get();
+			    	result += ( val.toString() + "-->" );
 			    }
-			    context.write(key, new IntWritable(sum));
+			    result = result.substring(0, result.length()-3);
+			    output.set(result);
+			    //System.out.println(output);
+			    context.write(key, output);
+			    //System.out.println(key+" "+output);
 			}
 	}
 
@@ -199,23 +277,49 @@ public class DocumentAnalysis {
 		Configuration conf = new Configuration();
 		conf.set("xmlinput.start", "<page>");
 		conf.set("xmlinput.end", "</page>");
-		
-		Job job = new Job(conf);
-		job.setJarByClass(DocumentAnalysis.class);
-		job.setJobName("document analysis");
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
-		
-		job.setMapperClass(DocumentAnalysis.Map.class);
-		job.setReducerClass(DocumentAnalysis.Reduce.class);
-		
-		job.setInputFormatClass(XmlInputFormat1.class);
-		job.setOutputFormatClass(TextOutputFormat.class);
-		
-		FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        
-        System.exit(job.waitForCompletion(true)?0:1);
+		Path path_tmp = new Path("hdfs://localhost:9000/output/job1_temp");
+		//job1
+		try
+		{
+			Job job1 = new Job(conf);
+			job1.setJarByClass(DocumentAnalysis.class);
+			job1.setJobName("InvertedIndex_Phase1");
+			job1.setOutputKeyClass(Text.class);
+			job1.setOutputValueClass(Text.class);
+
+			
+			
+			job1.setMapperClass(DocumentAnalysis.Map1.class);
+			job1.setReducerClass(DocumentAnalysis.Reduce1.class);
+			
+			job1.setInputFormatClass(XmlInputFormat1.class);
+			job1.setOutputFormatClass(SequenceFileOutputFormat.class);
+			FileInputFormat.addInputPath(job1, new Path(args[0]));
+			FileOutputFormat.setOutputPath(job1, path_tmp); 
+			job1.waitForCompletion(true);
+			
+			//job2
+			Job job2 = new Job(conf);
+			job2.setJarByClass(DocumentAnalysis.class);
+			job2.setJobName("InvertedIndex_Phase2");
+			job2.setInputFormatClass(SequenceFileInputFormat.class);
+			job2.setMapperClass(DocumentAnalysis.Map2.class);
+			job2.setReducerClass(DocumentAnalysis.Reduce2.class);
+			job2.setMapOutputKeyClass(Text.class);
+			job2.setMapOutputValueClass(Text.class);
+			Path job2_input = new Path("hdfs://localhost:9000/output/job1_temp/part-r-00000");
+			FileInputFormat.addInputPath(job2, job2_input);
+	        job2.setOutputKeyClass(Text.class);  
+	        job2.setOutputValueClass(Text.class);  
+	        job2.setOutputFormatClass(TextOutputFormat.class); 
+			
+	        FileOutputFormat.setOutputPath(job2, new Path(args[1]));
+	        job2.waitForCompletion(true);
+		}
+		finally
+		{
+			
+		}
 		
 	}
 }
